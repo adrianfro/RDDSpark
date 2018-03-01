112af750afa1e91d7c7962c915bdb936763f1182
aferreiro <aferreiro@aferreiro-ubuntu>
2018-03-01 20:34:42 +0100

    a

--- src/main/java/com/github/adrianfro/RDDSpark/operations/Level3.java
+++ src/main/java/com/github/adrianfro/RDDSpark/operations/Level3.java
@@ -1,5 +1,25 @@
 package com.github.adrianfro.RDDSpark.operations;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.distributed.DistributedMatrix;
+
 public class Level3 implements Operation {
+	
+	private static final Log LOG = LogFactory.getLog(Level3.class);
+	
+	/**
+	 * DGEMM
+	 * 
+	 * C := alpha * op(A) * op ( B ) + beta * C
+	 * 
+	 */
+	
+	public static DistributedMatrix dgemm(double alpha, DistributedMatrix A, DistributedMatrix B, double beta, DistributedMatrix C, JavaSparkContext context) {
+		
+		
+		return null;
+	}
 
 }
